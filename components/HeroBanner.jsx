21607b7308407213d7ce713e3e619cb904aa8c96
Matheus <mdmaia91@gmail.com>
2022-09-30 14:23:57 -0300

    babel fixed, and update homepage

---
+++ components/HeroBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HeroBanner = () => {
+  return (
+    
+    <div className='hero-banner-container'>
+      <div>
+        <p className='beats-solo'>SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className='hero-banner-image' />
+      </div>
+    </div>
+  )
+}
+
+export default HeroBanner
